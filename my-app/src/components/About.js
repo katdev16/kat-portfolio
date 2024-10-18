c5adbe5960125f37b65303366eca30cffc4a462a
katdev16 <katlegodhlamini2003@gmail.com>
2024-10-18 17:12:41 +0200

    about me update

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -8,6 +8,12 @@ const About = () => {
         Hi, I'm Katlego Dhlamini, a [Frontend/Backend/Full Stack] developer passionate about building interactive and
         dynamic web applications using various technologies.
       </p>
+      <p>
+        I am passionate about programming and transforming ideas into tangible software solutions.</p>
+        I continuously strive to enhance my skills and knowledge.
+       
+        My goal is to stay at the forefront
+        of technological advancements by embracing lifelong learning and professional development. I am dedicated to building efficient, scalable, and innovative applications that make a difference.
     </section>
   );
 };
--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <h2>Contact</h2>
       <p>Email: <a href="katlegodhlamini2003@gmail.com">katlegodhlamini2003@gmail.com</a></p>
       <p>GitHub: <a href="https://github.com/katdev16">github.com/katdev16</a></p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile">linkedin.com/in/yourprofile</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/katlego-dhlamini-932636284/">linkedin.com/in/yourprofile</a></p>
     </section>
   );
 };
