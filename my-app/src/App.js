4f94ecc153d20cc75bd17eecdd2d5ae78c696a87
katdev16 <katlegodhlamini2003@gmail.com>
2024-10-18 16:57:12 +0200

    images added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,14 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import './App.css'; // Create a simple CSS file for styling
+import profileImage from './images/profile.jpeg'; 
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Katlego Dhlamini - Developer Portfolio</h1>
+        <img src={profileImage} alt="Katlego Dhlamini" style={{ width: '150px', borderRadius: '50%' }} />
       </header>
       <About />
       <Skills />
