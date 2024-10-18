fc37a33abb17324d95b4aebb077419c95b5dfb3e
katdev16 <katlegodhlamini2003@gmail.com>
2024-10-18 16:29:57 +0200

    love the colors

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -5,20 +5,19 @@ const Projects = () => {
     <section id="projects">
       <h2>Projects</h2>
       <div className="project">
-        {/* <h3>WeShare App</h3>
+        <h3>Voice File Manger (Python)</h3>
         <p>
-          A full-stack social sharing platform using Java and Spring Boot, with Thymeleaf for the frontend and MySQL for
-          the database.
+          A file mangerment system using your voice 
         </p>
         <a href="https://github.com/yourusername/weshare-app">View on GitHub</a>
       </div>
-      <div className="project">
+      {/* <div className="project">
         <h3>RobotWorld API</h3>
         <p>
           Developed an API for a robot simulation project using Java, Javalin, and PostgreSQL.
         </p>
-        <a href="https://github.com/yourusername/robotworld-api">View on GitHub</a> */}
-      </div>
+        <a href="https://github.com/yourusername/robotworld-api">View on GitHub</a>
+      </div> */}
     </section>
   );
 };
